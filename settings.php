b964a0ca1b22ce5073cc08f8dcf4976f87026e70
Julian Gomez <ugomez@fluidattacks.com>
2023-03-27 22:34:05 -0500

    add missing slash

--- settings.php
+++ settings.php
@@ -13,7 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with SoundLab plugin for Moodle.  If not, see <http://www.gnu.org/licenses/>.
-require_once __DIR__ . "vendor/autoload.php";
+require_once __DIR__ . "/vendor/autoload.php";
 
 use duncan3dc\Speaker\TextToSpeech;
 use duncan3dc\Speaker\Providers\VoiceRssProvider;
