ce3bb2ca0e748167d88b0fbdad2edb52a1d3b98a
Julian Gomez <ugomez@fluidattacks.com>
2023-03-28 23:05:52 -0500

    Feat: base for tts

--- block_soundlab.php
+++ block_soundlab.php
@@ -22,6 +22,11 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+require_once __DIR__ . "/vendor/autoload.php";
+
+use duncan3dc\Speaker\TextToSpeech;
+use duncan3dc\Speaker\Providers\VoiceRssProvider;
+
 class block_soundlab extends block_base {
 
     /**
@@ -52,11 +57,11 @@ class block_soundlab extends block_base {
 
     public function instance_allow_config()
     {
-        return true;
+        return false;
     }
 
     public function has_config() {
-        return true;
+        return false;
     }
 
     /**
@@ -66,10 +71,10 @@ class block_soundlab extends block_base {
      */
     public function applicable_formats() {
         return [
-            'admin' => false,
+            'admin' => true,
             'site-index' => true,
             'course-view' => true,
-            'mod' => false,
+            'mod' => true,
             'my' => true,
         ];
     }
@@ -95,11 +100,24 @@ class block_soundlab extends block_base {
         }
 
         $this->content = new stdClass;
-        $ttsAppURL = $CFG->wwwroot . '/blocks/soundlab/app/';
+        $ttsAppURL = $CFG->wwwroot . '/blocks/soundlab/';
         $this->content->text = '<h5>Volumen</h5>
         <input type="range" id="volume" class="control-volume" min="0" max="100" value="75" step="1" data-action="volume" />
         <h5>Velocidad</h5>
         <input type="range" id="velocity" class="control-velocity" min="-10" max="10" value="2" step="1" data-action="velocity" />';
+        $this->content->text .= '<script> window.addEventListener("load", function() {
+            var pageContent = document.getElementById(\'responseform\').innerHTML;
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(pageContent , \'text/html\');
+            const bodyContents = doc.body.textContent.replace(/\n/g, " ");
+            var fecha = new Date();
+            fecha.setTime(fecha.getTime() + (60 * 60 * 1000)); // 1 hora
+            var expiracion = "expires=" + fecha.toUTCString();
+            document.cookie = "textoPaLeerGonorrea=" + bodyContents + "; expires=" + expiracion + "; path=/";
+        });</script>';
+        $textoPaLeer = $_COOKIE["textoPaLeerGonorrea"];
+        $provider = new VoiceRssProvider("6a42c931a8124001a4f6270db149d3de", "es-mx");
+        $tts = new TextToSpeech($textoPaLeer, $provider);
+        $tts->save("/var/moodledata/data.mp3", $tts->getAudioData());
     }
-
 }
